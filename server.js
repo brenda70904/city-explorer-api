36fada179ec0f304f3abe4ac1467f134fe3fa168
brenda70904 <brendaj0214@gmail.com>
2022-11-17 23:09:38 -0800

    add module

--- server.js
+++ server.js
@@ -7,66 +7,28 @@ app.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 
-let axios = require('axios');
 
-//////////////////
-app.get('/',(request, response) => {
+//home
+app.get('/', (request, response) => {
   response.send('hello from server');
 });
 
+//weather
+let weather = require('./weather');
+app.get('/weather', weather);
 
-app.get('/weather', async(request, response) => {
-  try {
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    let cityData = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
-    if(!cityData){
-      response.send('Cannot find the city.');
-    }
-    let cities = cityData.data.data.map(eachDateObj => new Forecast(eachDateObj));
-    response.send(cities);
-  } catch (error) {
-    response.send('Sorry not in the data');
-  }
-});
+//movie
+let movie = require('./movie');
+app.get('/movies', movie);
 
-app.get('/movies', async (request ,response) => {
-  try {
-    let name = request.query.query;
-    let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${name}`);
-    if(!movieData){
-      response.send('Cannot find the match.');
-    }
-    let movieResult = movieData.data.results.map(item => new Movie(item));
-    response.send(movieResult);//object
-  } catch (error) {
-    response.send(error.message);
-  }
-});
-
+//handle route
 app.get('*', (request, response) => {
   response.send('Route does not exist');
 });
-
 
 //handle any errors
 app.use((error, request, response) => {
   response.status(500).send(error.message);
 });
-
-class Forecast {
-  constructor(cityObject) {
-    this.description = `low of ${cityObject.low_temp}, hight of ${cityObject.max_temp} with ${cityObject.weather.description}`;
-    this.date = cityObject.datetime;
-  }
-}
-
-class Movie {
-  constructor(movieObject){
-    this.title = movieObject.original_title;
-    this.releaseDate = movieObject.release_date;
-    this.src = movieObject.poster_path;
-  }
-}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
